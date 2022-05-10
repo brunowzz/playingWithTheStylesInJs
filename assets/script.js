4887a3631ba5130046272b6e92a503d3cba65e36
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2022-05-10 16:28:29 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+function mudacor() {
+    document.getElementById('manipula').style.color = 'pink'
+}
+
+function cresceFonte() {
+    document.getElementById('manipula').style.fontSize = '50px'
+}
+
+function diminuiFonte() {
+    document.getElementById('manipula').style.fontSize = '27px'
+}
+
+function fontItalica() {
+    document.getElementById('manipula').style.fontStyle = 'italic'
+}
+
+function fontNegrito() {
+    document.getElementById('manipula').style.fontStyle = 'bold'
+}
+
+function backgroundRoxo() {
+    document.getElementById('manipula').style.backgroundColor = 'purple'
+}
+
+function esconder() {
+    document.getElementById('manipula').style.display = 'none'
+}
+
+function aparecer() {
+    document.getElementById('manipula').style.display = 'block'
+}
